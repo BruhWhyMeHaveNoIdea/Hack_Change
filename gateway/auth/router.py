bba715348d0f4443583644172922d2a202c57ec0
AlexCasella <lolkek308cheb@mail.ru>
2025-11-30 12:08:49 +0500

    update router.py

--- gateway/auth/router.py
+++ gateway/auth/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from auth.jwt import create_jwt, decode_jwt
 from db.crud.students import get_all_students
+import bcrypt
 
 
 router = APIRouter(prefix="/auth")
@@ -14,11 +15,16 @@ async def login(data: dict):
 
     students_by_email = {student["email"]: student for student in students}
 
-    if email not in students_by_email or students_by_email[email]["password"] != password:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+    if email not in students_by_email or bcrypt.checkpw(password, students_by_email[email]["password"]):
+        return {
+            "status": 401,
+            "jwtToken": "",
+            "refreshToken": ""
+        }
 
     student_id = students_by_email[email]["student_id"]
     return {
+        "status": 200,
         "jwtToken": create_jwt(student_id),
         "refreshToken": create_jwt(student_id)
     }
